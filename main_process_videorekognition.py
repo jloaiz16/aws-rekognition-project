927e742bb5bd17903fcd12d4c2b80a251e6df0eb
Jorge Ivan Ortiz <jortizs4@eafit.edu.co>
2018-06-07 17:53:24 -0500

    Main para compilacion del proyecto

---
+++ main_process_videorekognition.py
@@ -0,0 +1,40 @@
+import rekognition_video
+import streamvideo
+
+pointer = True
+
+if __name__ == '__main__' :
+
+	print("PROCESSING VIDEO SYSTEM v2.0")
+	print("Face recognition ...")
+	print("Escriba 1: Procesamiento video almacenado")
+	print("Escriba 2: Procesamiento video en vivo")
+
+	option = input()
+
+	if option == 1 :
+
+		file_vd = raw_input("Ingrese nombre de video a procesar con su extension: ")
+		while pointer :
+			print("Escriba 1: Obtener detalles del video")
+			print("Escriba 2: Verificar si se encuentra persona especifica")
+
+			option = input()
+
+			if option == 1 :
+				print("sd")
+				#rekognition_video.getvideodetails()
+			elif option == 2 :
+				file_im = raw_input("Ingrese nombre de imagen de persona a verificar: ")
+				#rekognition_video.verifyperson()
+			else :
+				print("Opcion incorrecta")
+
+
+			print("Escriba True: continuar | Escriba False: parar")
+			pointer = bool(input())
+		    #rekognition_video.build_reckognition(file_vd)
+	elif option == 2 :
+		streamvideo.getvideorekognition()
+	else :
+		print("Opcion incorrecta")
